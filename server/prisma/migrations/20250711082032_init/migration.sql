03c3226aa675bf48e66ccf897c544bf435a1ee57
Boburov Shukurullo <boburovdev@gmail.com>
2025-07-11 13:21:10 +0500

    wayprint started

---
+++ server/prisma/migrations/20250711082032_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER', 'CUSTOMER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "surname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
