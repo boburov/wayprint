19936f534e4b427646055bb4af93518de1da258f
Boburov Shukurullo <boburovdev@gmail.com>
2025-07-16 01:05:05 +0500

    fixed

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,10 +1,8 @@
-// app/components/Card.tsx
-
 import Image from "next/image";
 
 type CardProps = {
   title: string;
-  image: String;
+  image: string;
   rate: number | string;
   amout: number | string;
 };
@@ -12,7 +10,7 @@ type CardProps = {
 const Card = ({ title, image, rate, amout }: CardProps) => {
   return (
     <div className="text-xl border border-gray-400 rounded-md p-5">
-      <Image src={String(image)} alt={title} width={200} height={200} />
+      <Image src={image} alt={title} width={200} height={200} />
       <h1 className="text-base">{title}</h1>
       <p>{rate}</p>
       <p>{amout}</p>
